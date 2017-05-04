e2873be425605bb89d07f145113269fcc3afd00b
Ormat <Ormat@ormat.com>
2017-05-04 20:52:14 +1200

    Broadcast bitrate changed events - server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const HTTPS_PORT = 8080;
 const TYPE_INITIAL_HANDSHAKE = 0;
 const TYPE_SDP_CONNECTION = 1;
 const TYPE_ICE_INFO = 2;
+const TYPE_BITRATE_CHANGED_INFO = 3;
 
 const fs = require('fs');
 const https = require('https');
@@ -11,18 +12,21 @@ const WebSocket = require('ws');
 // SSL is required for the WebRTC connections for Chrome. We use a temporary self-signed certificate
 // https://tokbox.com/blog/the-impact-of-googles-new-chrome-security-policy-on-webrtc/
 const serverConfig = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem'),
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
 };
 
 // Create a server for the client html page
 var handleRequest = function(request, response) {
     if(request.url === '/') {
         response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end(fs.readFileSync('client/index.html'));
+        response.end(fs.readFileSync('../client/login.html'));
     } else if(request.url === '/app.js') {
         response.writeHead(200, {'Content-Type': 'application/javascript'});
-        response.end(fs.readFileSync('client/app.js'));
+        response.end(fs.readFileSync('../client/app.js'));
+    }else if(request.url === '/app') {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(fs.readFileSync('../client/index.html'));
     }
 };
 
@@ -49,6 +53,10 @@ wss.on('connection', function(ws) {
             break;
 
             case TYPE_ICE_INFO:
+            wss.broadcast(message);
+            break;
+
+            case TYPE_BITRATE_CHANGED_INFO:
             wss.broadcast(message);
             break;
         }
